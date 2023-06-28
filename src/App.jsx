f5ed534390465afa713db9c2672835afc273303e
Reese Chong <dev.reese.chong@gmail.com>
2023-06-28 19:57:35 -0400

    converted old tailwind component to antd

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import TodoList from './components/ToDo'
 import './App.css'
 
 function App() {
@@ -8,9 +7,7 @@ function App() {
 
   return (
     <>
-      <div>
-          <p>To do list</p>
-      </div>
+      <TodoList/>
     </>
   )
 }
